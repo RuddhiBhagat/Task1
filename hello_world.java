59494f58837675c3e06bd828654e6148b5299634
RuddhiBhagat <ruddhi.bhagat@atyeti.com>
2024-11-28 16:25:32 +0530

    fixed the error for secret keys

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,11 @@
 // HelloWorld.java
 public class hello_world {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+       if (args.length > 0) {
+            String secretKey = args[0];  // First argument is the secret key
+            System.out.println("The secret key is: " + secretKey);  // This will print the secret key in the console
+        } else {
+            System.out.println("No secret key provided.");
+        }
     }
 }
